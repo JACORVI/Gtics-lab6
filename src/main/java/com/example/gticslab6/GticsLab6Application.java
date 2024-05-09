4c1871072700a3d354788eaff34b3dacb55cd813
JACORVI <117043793+JACORVI@users.noreply.github.com>
2024-05-08 19:21:41 -0500

    asasd
    as

---
+++ src/main/java/com/example/gticslab6/GticsLab6Application.java
@@ -0,0 +1,13 @@
+package com.example.gticslab6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab6Application.class, args);
+    }
+
+}
